8ffe9644dc873babb8c8c660404df532a0d811bc
moullas <moullas@gmail.com>
2020-12-02 11:37:20 +0200

    AOC Day 2 - Final with measurements

--- 2020/day2.ps1
+++ 2020/day2.ps1
@@ -11,30 +11,30 @@ Write-Host "********************************************" -ForegroundColor Green
 
 Write-Host "Part 1" -ForegroundColor Yellow
 $validPasswords = 0
-$timer = Measure-Command{
-foreach ($dataLine in $inputData){
-    $resultCount = 0
-    $data = ($dataLine.Replace(":","")).Replace("-"," ").Split(" ")
-    $resultCount = ($data[3] | Select-String -Pattern $data[2] -AllMatches).Matches.Count
-    if (($resultCount -ge [int]$data[0]) -and ($resultCount -le [int]$data[1])){
-        $validPasswords++
+$timer = Measure-Command {
+    foreach ($dataLine in $inputData) {
+        $resultCount = 0
+        $data = ($dataLine.Replace(":", "")).Replace("-", " ").Split(" ")
+        $resultCount = ($data[3] | Select-String -Pattern $data[2] -AllMatches).Matches.Count
+        if (($resultCount -ge [int]$data[0]) -and ($resultCount -le [int]$data[1])) {
+            $validPasswords++
+        }
     }
 }
-}
-Write-Host "Valid Passwords Found: $validPasswords in $($timer.TotalMilliseconds) milliseconds"
+Write-Host "Found $validPasswords valid passwords in $([math]::Round($timer.TotalMilliseconds)) milliseconds"
 Write-Host "********************************************" -ForegroundColor Green
 
 Write-Host "Part 2" -ForegroundColor Yellow
 $validPasswords = 0
-$timer = Measure-Command{
-foreach ($dataLine in $inputData){
-    $resultCount = 0
-    $data = ($dataLine.Replace(":","")).Replace("-"," ").Split(" ")
-    $passPos1 = [int]($data[0]) - 1 #-1 to account for non index-zero
-    $passPos2 = [int]($data[1]) - 1
-    $pass = $data[3]
-    if (($pass.Substring($passPos1,1) -eq $data[2]) -xor ($pass.Substring($passPos2,1) -eq $data[2])){$validPasswords++}
+$timer = Measure-Command {
+    foreach ($dataLine in $inputData) {
+        $resultCount = 0
+        $data = ($dataLine.Replace(":", "")).Replace("-", " ").Split(" ")
+        $passPos1 = [int]($data[0]) - 1 #-1 to account for non index-zero
+        $passPos2 = [int]($data[1]) - 1
+        $pass = $data[3]
+        if (($pass.Substring($passPos1, 1) -eq $data[2]) -xor ($pass.Substring($passPos2, 1) -eq $data[2])) { $validPasswords++ }
+    }
 }
-}
-Write-Host "Valid Passwords Found: $validPasswords in $($timer.TotalMilliseconds) milliseconds"
+Write-Host "Found $validPasswords valid passwords in $([math]::Round($timer.TotalMilliseconds)) milliseconds"
 Write-Host "********************************************" -ForegroundColor Green
