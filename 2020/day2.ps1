1c01d1f1e7aaa847e18fd512e4fe91042c2e744e
moullas <moullas@gmail.com>
2020-12-02 11:27:49 +0200

    AOC Day 2 cleanup

--- 2020/day2.ps1
+++ 2020/day2.ps1
@@ -1,45 +1,40 @@
 $aocYear = "2020"
 $aocDay = 2
+$inputFile = "day2input.txt"
 Set-Location $PSScriptRoot
 Write-Host "********************************************" -ForegroundColor Green
 Write-Host "* Advent of Code $aocYear - Solution for Day $aocDay *" -ForegroundColor Yellow
 Write-Host "********************************************" -ForegroundColor Green
-#https://adventofcode.com/2020/day/2/input
-
-$inputData = Get-Content day2input.txt
-Write-Host "Loaded $($inputData.Count) entries"
-$validPasswords = 0
+$inputData = Get-Content $inputFile
+Write-Host "Loaded $($inputData.Count) entries..."
 Write-Host "********************************************" -ForegroundColor Green
+
 Write-Host "Part 1" -ForegroundColor Yellow
+$validPasswords = 0
+$timer = Measure-Command{
 foreach ($dataLine in $inputData){
     $resultCount = 0
     $data = ($dataLine.Replace(":","")).Replace("-"," ").Split(" ")
-    [int]$passLow = $data[0]
-    [int]$passHigh = $data[1]
-    $pass = $data[3]
-    $result = $pass | Select-String -Pattern $data[2] -AllMatches
-    $resultCount = $result.Matches.Count
-    if (($resultCount -ge $passLow) -and ($resultCount -le $passHigh)){
+    $resultCount = ($data[3] | Select-String -Pattern $data[2] -AllMatches).Matches.Count
+    if (($resultCount -ge [int]$data[0]) -and ($resultCount -le [int]$data[1])){
         $validPasswords++
     }
 }
-Write-Host "Valid Passwords Found: $validPasswords"
+}
+Write-Host "Valid Passwords Found: $validPasswords in $($timer.TotalMilliseconds) milliseconds"
 Write-Host "********************************************" -ForegroundColor Green
 
 Write-Host "Part 2" -ForegroundColor Yellow
 $validPasswords = 0
+$timer = Measure-Command{
 foreach ($dataLine in $inputData){
     $resultCount = 0
     $data = ($dataLine.Replace(":","")).Replace("-"," ").Split(" ")
     $passPos1 = [int]($data[0]) - 1 #-1 to account for non index-zero
     $passPos2 = [int]($data[1]) - 1
     $pass = $data[3]
-    $result1 = $pass.Substring($passPos1,1) -eq $data[2]
-    $result2 = $pass.Substring($passPos2,1) -eq $data[2]
-
-    if ($result1 -xor $result2){
-        $validPasswords++
-    }
+    if (($pass.Substring($passPos1,1) -eq $data[2]) -xor ($pass.Substring($passPos2,1) -eq $data[2])){$validPasswords++}
 }
-Write-Host "Valid Passwords Found: $validPasswords"
+}
+Write-Host "Valid Passwords Found: $validPasswords in $($timer.TotalMilliseconds) milliseconds"
 Write-Host "********************************************" -ForegroundColor Green
