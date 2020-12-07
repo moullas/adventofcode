8eb20a7ec8d0a3928f7befd31c3426af13feb6cf
moullas <moullas@gmail.com>
2020-12-07 23:24:54 +0200

    AOC Day 6 Done

--- 2020/day6.ps1
+++ 2020/day6.ps1
@@ -12,27 +12,36 @@ Write-Host "********************************************" -ForegroundColor Green
 Write-Host "Part 1" -ForegroundColor Yellow
 
 $timer = Measure-Command {
-
-
-
-
-
-
-
-
-
-
-
-
+    $groupAnswers = @()
+    $form = $null
+    $totalYes = 0
+    foreach ($line in $inputData) {
+        if ($line.Length -gt 0) {
+            $form += $line.Replace("`n", "") + " "
+        }
+        else {
+            $groupAnswers += $form
+            $totalYes += ($form.Replace(" ", "").ToCharArray() | Sort-Object | Get-Unique).Count
+            $form = $null
+        }
+    }
+    Write-Host "The sum of all counts is $totalYes" -ForegroundColor Green
 
 }
 Write-Host "Elapsed time $([math]::Round($timer.TotalMilliseconds)) milliseconds"
-
-<#
 Write-Host "********************************************" -ForegroundColor Green
+
 Write-Host "Part 2" -ForegroundColor Yellow
+
 $timer = Measure-Command {
+    $allAnswers = 0
+    foreach ($form in $groupAnswers) {
+        $line = $form.Trim().Split(" ")
+        $matches = $line.ToCharArray() | Group | Where { $_.Name -in "a".."z" -and $_.Count -eq $line.Count }
+        $allAnswers += $matches.Name.Count
+    }
 }
+Write-Host "Found $allanswers answers"
+
 Write-Host "Elapsed time $([math]::Round($timer.TotalMilliseconds)) milliseconds"
 Write-Host "********************************************" -ForegroundColor Green
-#>
